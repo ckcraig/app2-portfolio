5daed566c47821249a38023cebda6683a71fbfb5
Christopher Craig <ckcraig@outlook.com>
2024-07-21 22:07:58 -0700

    Adding other elements from data.txt. - Day 22

--- main.py
+++ main.py
@@ -28,17 +28,20 @@ tag = """
     """
 st.write(tag)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
-
-    st.image("images/1.png")
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
-    st.image("images/2.png")

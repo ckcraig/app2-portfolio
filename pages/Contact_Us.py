c20738da83518c8b8146e3669dcfeab4bcb2b85e
Christopher Craig <ckcraig@outlook.com>
2024-07-22 21:47:55 -0700

    Sending emails via form. - Day 23

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from send_email import send_email
+import email.header
+
 
 st.set_page_config(layout="wide")
 
@@ -6,8 +9,16 @@ st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message here")
+    raw_message = st.text_area("Your message here")
+
+    message = f"""\
+Subject: New email from Portfolio App | {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     submit_button = st.form_submit_button("Send Message")
     if submit_button:
-        # message = message + user_email
         print("Pressed successfully")
+        send_email(message)
+        st.info("Your email was successfully sent!")
--- send_email.py
+++ send_email.py
@@ -1,21 +1,16 @@
 import smtplib, ssl
 
 
-host = "smtp.gmail.com"
-port = 465
-context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
 
-username = "luciusfremon@gmail.com"
-password = "thqdidpzbkxacvgb"
+    username = "luciusfremon@gmail.com"
+    password = "thqdidpzbkxacvgb"
 
-receiver = "ckcraig2006@gmail.com"
+    receiver = "ckcraig2006@gmail.com"
 
-message = """\
-Subject: Email from Portfolio App
-Hi!
-This is a new message.
-"""
-
-with smtplib.SMTP_SSL(host, port, context=None) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=None) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
